a269990966902d8d67c97508c4d269cc657e855b
imranrehmani02 <mo.imranrehmani02@gmail.com>
2020-12-19 17:41:23 +0530

    commit 2

--- server/model/db.js
+++ server/model/db.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var url = "mongodb+srv://imranrehmani02:qIeojwuyZZkO40zO@cluster0.n3awq.mongodb.net/<dbname>?retryWrites=true&w=majority";
+var url = "mongodb+srv://imranrehmani02:qIeojwuyZZkO40zO@cluster0.n3awq.mongodb.net/bloodbank?retryWrites=true&w=majority";
 
 function getConnection(callback)
 {
